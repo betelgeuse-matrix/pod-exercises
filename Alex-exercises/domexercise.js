a9d6a9c039ddc9e4e73edc8c00262f9c8ab4607a
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-07-18 19:48:36 -0500

    update to my html pictures and created directory for pictures

--- Alex-exercises/domexercise.js
+++ Alex-exercises/domexercise.js
@@ -17,43 +17,43 @@ console.log(duplicateEncode('hello'));
 
 // get pictures to swap
 //
-// $("#swap1").click(function () {
-//     var imgLeft = $("#1").attr("src");
-//     var imgCenter = $("#2").attr("src");
+$("#swap1").click(function () {
+    var imgLeft = $("#1").attr("src");
+    var imgCenter = $("#2").attr("src");
+
+
+    $("#1").attr("src",imgCenter);
+    $("#2").attr('src',imgLeft);
+});
+
+
+$("#swap3").click(function () {
+    var imgCenter = $("#2").attr("src");
+    var imgRight = $("#3").attr("src");
+
+    $("#3").attr("src", imgCenter);
+    $("#2").attr("src", imgRight);
+});
 //
 //
-//     $("#1").attr("src",imgCenter);
-//     $("#2").attr('src',imgLeft);
-// });
-//
-//
-// $("#swap3").click(function () {
-//     var imgCenter = $("#2").attr("src");
-//     var imgRight = $("#3").attr("src");
-//
-//     $("#3").attr("src", imgCenter);
-//     $("#2").attr("src", imgRight);
-// });
-// //
-// //
-// $("#swap2").click(function(){
-//
-//     for(var i=2; i>=0; i--) {
-//         var imgLeft = $("#1").attr("src");
-//         var imgCenter = $("#2").attr("src");
-//         var imgRight = $("#3").attr("src");
-//
-//         var randomNumber = Math.floor(Math.random() * i);
-//
-//         if (randomNumber === 0) {
-//             $("#2").attr("src", imgLeft);
-//             $("#1").attr("src", imgCenter)
-//         } else {
-//             $("#2").attr("src", imgRight);
-//             $("#3").attr("src", imgCenter)
-//         }
-//     }
-// });
+$("#swap2").click(function(){
+
+    for(var i=2; i>=0; i--) {
+        var imgLeft = $("#1").attr("src");
+        var imgCenter = $("#2").attr("src");
+        var imgRight = $("#3").attr("src");
+
+        var randomNumber = Math.floor(Math.random() * i);
+
+        if (randomNumber === 0) {
+            $("#2").attr("src", imgLeft);
+            $("#1").attr("src", imgCenter)
+        } else {
+            $("#2").attr("src", imgRight);
+            $("#3").attr("src", imgCenter)
+        }
+    }
+});
 
 
 
